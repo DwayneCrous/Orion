db137ac153f06d1631026c165a63330d9bbd7f49
Dwayne <91212710+DwayneCrous@users.noreply.github.com>
2025-07-26 11:01:30 +0200

    defer reply added

--- src/commands/random-joke.js
+++ src/commands/random-joke.js
@@ -11,6 +11,7 @@ module.exports = {
     .setName("random-joke")
     .setDescription("Replies with a random joke"),
   async execute(interaction) {
+    await interaction.deferReply();
     try {
       const response = await fetchImpl(
         "https://official-joke-api.appspot.com/random_joke"
@@ -21,10 +22,10 @@ module.exports = {
         );
       const data = await response.json();
       const joke = `${data.setup}\n${data.punchline}`;
-      await interaction.reply(joke);
+      await interaction.editReply(joke);
     } catch (error) {
       console.error("Random Joke Command Error:", error);
-      await interaction.reply(
+      await interaction.editReply(
         "Sorry, I couldn't fetch a joke right now.\n" + (error.message || error)
       );
     }
