ed1bda4b1791b1a315bbb4d21a2b9e11c35db761
Dwayne <91212710+DwayneCrous@users.noreply.github.com>
2025-07-26 19:01:23 +0200

    temporarily muted orion

--- src/index.js
+++ src/index.js
@@ -220,19 +220,19 @@ client.on("messageCreate", async (message) => {
     return;
   }
 
-  let member;
-  try {
-    member = await message.guild.members.fetch(message.author.id);
-  } catch {
-    return;
-  }
-  const presence = member.presence;
-  const status = presence?.status;
-  if (!presence || ["offline", "idle", "dnd"].includes(status)) {
-    await message.reply(
-      "https://cdn.discordapp.com/attachments/1269017675544268852/1396456663027941497/5ltbz8.png?ex=687e271d&is=687cd59d&hm=1fccf934b7ff80b5b9392191bdc659b286ae723b73158464109241a84f5ed613&"
-    );
-  }
+  // let member;
+  // try {
+  //   member = await message.guild.members.fetch(message.author.id);
+  // } catch {
+  //   return;
+  // }
+  // const presence = member.presence;
+  // const status = presence?.status;
+  // if (!presence || ["offline", "idle", "dnd"].includes(status)) {
+  //   await message.reply(
+  //     "https://cdn.discordapp.com/attachments/1269017675544268852/1396456663027941497/5ltbz8.png?ex=687e271d&is=687cd59d&hm=1fccf934b7ff80b5b9392191bdc659b286ae723b73158464109241a84f5ed613&"
+  //   );
+  // }
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
